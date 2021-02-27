b8fcdeb67811fcfb768537933d3f4fa47e323041
HJackH <56967377+HJackH@users.noreply.github.com>
2021-02-28 01:19:31 +0800

    Set tab size to 2

--- Contents/2_Data_Structure/2_BIT_RARSQ.cpp
+++ Contents/2_Data_Structure/2_BIT_RARSQ.cpp
@@ -5,26 +5,26 @@ int n;
 vector<int> B1, B2;
 
 void add(vector<int> &tr, int id, int val) {
-    for (; id <= n; id += lowbit(id)) {
-        tr[id] += val;
-    }
+  for (; id <= n; id += lowbit(id)) {
+    tr[id] += val;
+  }
 }
 void range_add(int l, int r, int val) {
-    add(B1, l, val);
-    add(B1, r + 1, -val);
-    add(B2, l, val * (l - 1));
-    add(B2, r + 1, -val * r);
+  add(B1, l, val);
+  add(B1, r + 1, -val);
+  add(B2, l, val * (l - 1));
+  add(B2, r + 1, -val * r);
 }
 int sum(vector<int> &tr, int id) {
-    int ret = 0;
-    for (; id >= 1; id -= lowbit(id)) {
-        ret += tr[id];
-    }
-    return ret;
+  int ret = 0;
+  for (; id >= 1; id -= lowbit(id)) {
+    ret += tr[id];
+  }
+  return ret;
 }
 int prefix_sum(int id) {
-    return sum(B1, id) * id - sum(B2, id);
+  return sum(B1, id) * id - sum(B2, id);
 }
 int range_sum(int l, int r) {
-    return prefix_sum(r) - prefix_sum(l - 1);
+  return prefix_sum(r) - prefix_sum(l - 1);
 }
